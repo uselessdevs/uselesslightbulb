fb2cdde8c03e7cadd4fbfeb33d14c81fea1f5dfd
uselessdevs <uselessdevs@gmail.com>
2018-08-04 18:31:09 -0300

    first commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var Uselesslightbulb = artifacts.require('./Uselesslightbulb.sol');
+
+module.exports = function(deployer) {
+  deployer.deploy(Uselesslightbulb);
+};
---
+++ truffle.js
@@ -0,0 +1,26 @@
+// Allows us to use ES6 in our migrations and tests.
+require('babel-register');
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraApikey = 'aeaa556522444ac3a89a949a5b752f46';
+const mnemonic = 'doll matrix wish party dragon sibling day solve jungle nest fame theme';
+
+module.exports = {
+  migrations_directory: "./migrations",
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      //port: 8545,
+      port: 8545,
+      network_id: '*', // Match any network id
+      gas: 2000000,
+      gasLimit: 2100000,
+      gasPrice: 18000000000,
+    },
+    rinkeby: {
+      provider: function () {
+        return new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraApikey}`);
+      },
+      network_id: 4,
+    }
+  },
+};
